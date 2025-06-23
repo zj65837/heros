183b41046df0f35983faabc057eccad222c6c70c
stone <70825994+zj65837@users.noreply.github.com>
2025-06-23 23:15:45 +0800

    Update chooseServer.go
    注释一下

--- loginutil/chooseServer.go
+++ loginutil/chooseServer.go
@@ -9,22 +9,33 @@ import (
 	"fmt"
 )
 
+// 定义服务器 URL 的结构体
 type urlStruct struct {
-	QuickLoginUrl string
-	GameUrl       string
+	QuickLoginUrl string // 快速登录 URL
+	GameUrl       string // 游戏服务器 URL
 }
 
+// 全局变量：存储服务器列表信息
 var srv map[string]interface{}
+
+// 全局变量：存储区服代码到 URL 的映射
 var rt = make(map[string]urlStruct)
 
+// 定义服务器主机地址列表的类型
 type BZSRVLIST map[string][]string
 
+// 全局变量：存储服务器主机地址列表
 var bsvrlst BZSRVLIST
 
+// ChooseServer 根据区服代码选择对应的服务器 URL
+// 参数 servercode: 区服代码 (如 "g1", "h2" 等)
+// 返回值: 快速登录URL, 游戏URL, 错误信息
 func ChooseServer(servercode string) (string, string, error) {
+	// 遍历服务器列表 srv
 	for k, v := range srv {
+		// 获取服务器信息中的第6个元素（索引5）作为区服名称
 		switch v.([]interface{})[5].(string) {
-
+		// 根据区服名称映射到对应的区服代码
 		case "官方1区":
 			rt["g1"] = urlStruct{QuickLoginUrl: fmt.Sprintf("%s:%s", bsvrlst[k][0], v.([]interface{})[2].(string)), GameUrl: fmt.Sprintf("%s:%s", bsvrlst[k][0], v.([]interface{})[3].(string))}
 		case "官方2区":
@@ -58,10 +69,13 @@ func ChooseServer(servercode string) (string, string, error) {
 		case "混服13区":
 			rt["h13"] = urlStruct{QuickLoginUrl: fmt.Sprintf("%s:%s", bsvrlst[k][0], v.([]interface{})[2].(string)), GameUrl: fmt.Sprintf("%s:%s", bsvrlst[k][0], v.([]interface{})[3].(string))}
 
+			// 未知区服名称处理
 		default:
 			return "", "", errors.New("[-] 区服代码错误.")
 		}
 	}
+	
+	// 返回请求的区服代码对应的 URL
 	return rt[fmt.Sprintf("%s", servercode)].QuickLoginUrl, rt[fmt.Sprintf("%s", servercode)].GameUrl, nil
 
 }
